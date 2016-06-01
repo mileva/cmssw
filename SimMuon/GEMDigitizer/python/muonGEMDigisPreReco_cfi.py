8e74bd46181af56c75e78c58847e1ccd02e13f7b
archie sharma <archiesharma12@gmail.com>
2016-06-01 11:46:20 +0200

    new changes for stations

--- SimMuon/GEMDigitizer/python/muonGEMDigisPreReco_cfi.py
+++ SimMuon/GEMDigitizer/python/muonGEMDigisPreReco_cfi.py
@@ -4,19 +4,22 @@ import FWCore.ParameterSet.Config as cms
 simMuonGEMDigiswithTiming = cms.EDProducer("GEMDigiPreRecoProducer",
     inputCollection = cms.string('g4SimHitsMuonGEMHits'),
     digiPreRecoModelString = cms.string('PreRecoGaussian'),
-    timeResolution = cms.double(0.001), # in ns
-    phiResolution = cms.double(0.05),   # in cm average resolution along local x in case of no correlation
+    #timeResolution = cms.double(0.001), # in ns
+    timeResolution = cms.double(5.0), # in ns
+    phiResolution = cms.double(0.05),   # in cm average resolution along local x in case of no correlation....for station 1..always fixed
+    phiResolution1 = cms.double(0.05),   # 0.05, 0.025, 0.01, 0.1 in cm average resolution along local x in case of no correlation...for station 2 & 3 
     etaResolution = cms.double(1.),     # in cm average resolution along local y in case of no correlation
     useCorrelation = cms.bool(False),
     useEtaProjectiveGEO = cms.bool(False),
+    constantPhiSpatialResolution = cms.bool(False),
     averageEfficiency = cms.double(0.98),
     digitizeOnlyMuons = cms.bool(True),
     gaussianSmearing = cms.bool(True),
     doBkgNoise = cms.bool(True), #False == No background simulation
     # simulateIntrinsicNoise = cms.bool(False), # intrinsic noise --> not implemented
     # averageNoiseRate = cms.double(0.001),     # intrinsic noise --> not implemented
-    simulateElectronBkg = cms.bool(False),       # True - will simulate electron background
-    simulateNeutralBkg  = cms.bool(False),       # True - will simulate neutral (n+g)  background
+    simulateElectronBkg = cms.bool(True),       # True - will simulate electron background
+    simulateNeutralBkg  = cms.bool(True),       # True - will simulate neutral (n+g)  background
     minBunch = cms.int32(-5),                   # [x 25 ns], forms the readout window together with maxBunch,
     maxBunch = cms.int32(3),                    # we should think of shrinking this window ...
 )
